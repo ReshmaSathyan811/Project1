d43e8f39144da14717cba172dfae76f7f1087b42
ReshmaSathyan <reshusathyan@gmail.com>
2022-05-03 11:36:25 +0530

    commit message

---
+++ app/src/test/java/com/resh/myapplicationmarch/ImageAdapter.java
@@ -0,0 +1,47 @@
+package com.resh.myapplicationmarch;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+    public Integer[] thumbImages= {
+            R.drawable.folder, R.drawable.settings, R.drawable.camera,
+            R.drawable.whatsup, R.drawable.phone, R.drawable.home, R.drawable.message
+    };
+
+    public ImageAdapter(Context c){
+        mContext=c;
+    }
+    public int getCount() {
+        return thumbImages.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+
+        ImageView imageView= new ImageView(mContext);
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(200, 200));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setPadding(8,8,8,8);
+        imageView.setImageResource(thumbImages[position]);
+        return imageView;
+    }
+}
